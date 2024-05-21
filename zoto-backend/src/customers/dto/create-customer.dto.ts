f2355d18f6b068b2a0756d66f44935165508840d
naoya533 <naoya533@gmail.com>
2024-05-21 11:56:25 +0900

    DTO整理

--- zoto-backend/src/customers/dto/create-customer.dto.ts
+++ zoto-backend/src/customers/dto/create-customer.dto.ts
@@ -1,9 +1,25 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  @IsString()
+  @IsNotEmpty()
   nameKana: string;
+  @IsString()
+  @IsNotEmpty()
   address: string;
+  @IsString()
+  @IsNotEmpty()
   postalCode: string;
+  @IsString()
+  @IsNotEmpty()
   email?: string;
+  @IsString()
+  @IsNotEmpty()
   phone: string;
+  @IsString()
+  @IsNotEmpty()
   companyCode: string;
 }
--- zoto-backend/src/customers/entities/customer.entity.ts
+++ zoto-backend/src/customers/entities/customer.entity.ts
@@ -1,1 +1,21 @@
-export class Customer {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Customer {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  name: string;
+  @Column()
+  nameKana: string;
+  @Column()
+  address: string;
+  @Column()
+  postalCode: string;
+  @Column()
+  email: string;
+  @Column()
+  phone: string;
+  @Column()
+  companyCode: string;
+}
