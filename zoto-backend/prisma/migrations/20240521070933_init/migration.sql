ad7310efb54f07979aa859ef1ebb64a3108dc9d5
naoya533 <naoya533@gmail.com>
2024-05-21 16:16:27 +0900

    マイグレーションファイル削除

--- zoto-backend/prisma/migrations/20240521070933_init/migration.sql
+++ zoto-backend/prisma/migrations/20240521070933_init/migration.sql
@@ -5,12 +5,25 @@ CREATE TABLE "Customer" (
     "nameKana" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "postalCode" TEXT NOT NULL,
-    "email" TEXT,
-    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
     "companyCode" TEXT NOT NULL,
 
     CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "companyCode" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Customer_email_key" ON "Customer"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
